6c45ed6d4b67c400e34462ce4bead374e7007667
Samuel Chin <samuelchin91@gmail.com>
2023-06-19 23:42:35 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,22 @@
 # Exit if any subcommand fails
 set -e
 
+# Save current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
 # Build the site
 bundle exec jekyll build
 
 # Switch to the gh-pages branch
 git checkout gh-pages
 
+# Remove all files under version control
+git rm -rf .
+
 touch .nojekyll
 
-# Move the built site to the root
-rsync -avr --exclude='.git' --exclude='deploy.sh' _site/ .
+# Copy the built site over
+cp -r ../_site/* .
 
 # Add, commit, and push the changes
 git add --all
@@ -20,4 +26,5 @@ git commit -m "Deploy site to gh-pages"
 git push origin gh-pages
 
 # Switch back to the original branch
-git checkout -
+git checkout $current_branch
+
